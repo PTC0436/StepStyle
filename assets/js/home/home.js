69c94283a8280c583874423159bb7b623ef71032
PTC0436 <phatthanhchau123@gmail.com>
2026-03-02 21:04:04 +0700

    Add banner to home

--- assets/js/home/home.js
+++ assets/js/home/home.js
@@ -1,3 +1,28 @@
-function showAlert() {
-  alert("Cảm ơn bạn đã quan tâm STEPSTYLE");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  function showAlert() {
+    alert("Cảm ơn bạn đã quan tâm STEPSTYLE");
+  }
+  window.showAlert = showAlert;
+
+  let slideIndex = 0;
+
+  const slides = document.querySelector(".slides");
+  const images = document.querySelectorAll(".slides img");
+
+  if (!slides || images.length === 0) {
+    console.log("Không tìm thấy slider hoặc hình ảnh");
+    return;
+  }
+
+  function nextSlide() {
+    slideIndex++;
+
+    if (slideIndex >= images.length) {
+      slideIndex = 0;
+    }
+
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+  }
+
+  setInterval(nextSlide, 3000);
+});
