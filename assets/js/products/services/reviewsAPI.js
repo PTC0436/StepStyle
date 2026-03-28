499015f2ea557ccf70e238d2f428cb71f3754d6a
PTC0436 <phatthanhchau123@gmail.com>
2026-03-28 09:38:35 +0700

    Add some comment 3

--- assets/js/products/services/reviewsAPI.js
+++ assets/js/products/services/reviewsAPI.js
@@ -1,5 +1,6 @@
 import api from "../../utils/request.js";
 
+//Lấy reviews theo id
 export const getReviewsByShoeId = async (shoeId) => {
   const res = await api.get(`/api/reviews/shoe/${shoeId}`);
   return res;
